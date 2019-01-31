00ec52d320419e7fc12ecc3dc760e44571e56c59
José Miguel Pérez Méndez <jotaemepm@outlook.com>
2019-01-31 01:58:19 -0300

    Init Clase https://www.udemy.com/angular-2-fernando-herrera/learn/v4/t/lecture/6427860?start=0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HeroesService } from "./Services/HeroesService";
+
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/Share/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { HeroesComponent } from './Components/heroes/heroes.component';
+import { HeroeComponent } from './Components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { HeroesComponent } from './Components/heroes/heroes.component';
+import { HeroeComponent } from './Components/heroe/heroe.component';
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent},
+    { path: 'heroes', component: HeroesComponent},
+    { path: 'heroe/:id', component: HeroeComponent},
+    { path: '**', pathMatch: 'full', component: HomeComponent }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+
+
+
